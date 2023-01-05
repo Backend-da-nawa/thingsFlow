7076f3bc7e460298ceae3e80d70537bdd90a259a
seungsu <h970126@gmail.com>
2023-01-05 09:31:47 +0900

    Initialize Project

---
+++ src/main/java/com/example/thinksFlow/ThinksFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.thinksFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinksFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinksFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thinksFlow/ThinksFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thinksFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinksFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
