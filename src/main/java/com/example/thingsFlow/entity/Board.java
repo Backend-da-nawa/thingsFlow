c2e7623aa7b7d551fb7cc96c2fa6497cd472257d
dong yeol <ehfql6363@naver.com>
2023-01-05 16:41:13 +0900

    FEAT(Board.java) : Board 엔티티 생성 및 속성 추가
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/entity/Board.java
+++ src/main/java/com/example/thingsFlow/entity/Board.java
@@ -1,4 +1,27 @@
 package com.example.thingsFlow.entity;
 
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@RequiredArgsConstructor
+@Table
 public class Board {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime time;
 }
