4a5ba746858b76bd3125db4304fd3ea62089eb9c
dong yeol <ehfql6363@naver.com>
2023-01-05 17:05:06 +0900

    FEAT(LoadController.java) : load api 작성
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/controller/LoadController.java
+++ src/main/java/com/example/thingsFlow/controller/LoadController.java
@@ -1,11 +1,22 @@
 package com.example.thingsFlow.controller;
 
-import lombok.NoArgsConstructor;
+import com.example.thingsFlow.entity.Board;
+import com.example.thingsFlow.service.LoadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class LoadController {
 
+    private LoadService loadService;
 
+    @GetMapping("/load/{page}")
+    public List<Board> load(@PathVariable int page) {
+
+    }
 }
