0ee3ba34140b7c6a6a9a1f4815eb53a0dcc45879
dong yeol <ehfql6363@naver.com>
2023-01-07 03:20:28 +0900

    FEAT(LoadDTO.java) : DTO 변수 및 생성자 생성

--- src/main/java/com/example/thingsFlow/dto/LoadDTO.java
+++ src/main/java/com/example/thingsFlow/dto/LoadDTO.java
@@ -1,4 +1,26 @@
 package com.example.thingsFlow.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@AllArgsConstructor
 public class LoadDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String password;
+    private LocalDateTime time;
+
+    public LoadDTO(Long id, String title, String content, String password) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.password = password;
+    }
 }
