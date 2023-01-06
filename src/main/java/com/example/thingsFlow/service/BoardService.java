d0decb71be53d7b2128e96e68945c740dec57a7a
jojeongbin <cjb1009@naver.com>
2023-01-06 22:13:57 +0900

    FEAT(BoardService) : updateBoard 메소드 추가

--- src/main/java/com/example/thingsFlow/service/BoardService.java
+++ src/main/java/com/example/thingsFlow/service/BoardService.java
@@ -1,4 +1,46 @@
 package com.example.thingsFlow.service;
 
+import com.example.thingsFlow.dto.UpdateDTO;
+import com.example.thingsFlow.entity.Board;
+import com.example.thingsFlow.repository.BoardRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Map;
+import java.util.Optional;
+
+import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
+
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long updateBoard(Long id, String password) throws Exception {
+        try {
+            Optional<Board> optional= boardRepository.findById(id);
+            Board oldBoard ;
+            if(optional==oldBoard.password);//oldBoard의 password와 optional의 password가 같으면 수정하기
+            else  throw  new Exception();
+
+            UpdateDTO updateDTO = UpdateDTO.builder()
+                    .id(id)
+                    .title(String.valueOf(title))
+                    .password(password)
+                    .build();
+            Board newBoard= Board.builder().updateDTO(updateDTO).build();
+            boardRepository.save(newBoard);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalAccessException("에러메세지");
+        }
+        return 1L;
+    }
+
+
+
 }
