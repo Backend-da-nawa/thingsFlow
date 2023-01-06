6e594d345475ceea7a9b0d9abfe6d26d01c48d1c
dong yeol <ehfql6363@naver.com>
2023-01-06 09:26:18 +0900

    FEAT(LoadController.java) : load 메서드 생성

--- src/main/java/com/example/thingsFlow/controller/LoadController.java
+++ src/main/java/com/example/thingsFlow/controller/LoadController.java
@@ -2,21 +2,20 @@ package com.example.thingsFlow.controller;
 
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.service.LoadService;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class LoadController {
 
     private LoadService loadService;
 
-    @GetMapping("/load/{page}")
-    public List<Board> load(@PathVariable int page) {
-
+    @GetMapping("/load")
+    public Page<Board> load(final Pageable pageable) {
+        return loadService.load(pageable);
     }
 }
