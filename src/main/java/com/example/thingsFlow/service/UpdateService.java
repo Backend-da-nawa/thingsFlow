0726528f65eefd9be6e80f7e3175ca0af69ef15d
jojeongbin <cjb1009@naver.com>
2023-01-07 03:13:26 +0900

    FEAT(UpdateService.java) : validatePassword 메소드 추가, password 검증 코드 추가

--- src/main/java/com/example/thingsFlow/service/UpdateService.java
+++ src/main/java/com/example/thingsFlow/service/UpdateService.java
@@ -3,38 +3,43 @@ package com.example.thingsFlow.service;
 import com.example.thingsFlow.dto.UpdateDTO;
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
-import lombok.AllArgsConstructor;
+import com.example.thingsFlow.validation.UpdateValidation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Map;
 import java.util.Optional;
 
-import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
-
 
 @Service
 @RequiredArgsConstructor
-public class BoardService {
+public class UpdateService {
+    @Autowired
     private final BoardRepository boardRepository;
 
+    @Autowired
+    private final UpdateValidation updateValidation;
+
     @Transactional
-    public Long updateBoard(Long id,String password) throws Exception {
+    public Long updateBoard(UpdateDTO updateDTO) throws Exception {
         try {
-            Optional<Board> optional= boardRepository.findById(id);
+            Optional<Board> optional = boardRepository.findById(updateDTO.getId());
             Board oldBoard;
-            if(optional.isPresent()){
-                oldBoard=optional.get();
-            } else  throw  new Exception();
+            if (optional.isPresent()) {
+                oldBoard = optional.get();
+            } else throw new Exception();
 
+            updateValidation.validatePassword(oldBoard, updateDTO);
 
-            UpdateDTO updateDTO = UpdateDTO.builder()
-                    .id(id)
-                    .title()
-                    .password(password)
+            UpdateDTO newDTO = UpdateDTO.builder()
+                    .id(oldBoard.getId())
+                    .title(updateDTO.getTitle())
+                    .content(updateDTO.getContent())
+                    .password(oldBoard.getPassword())
                     .build();
-            Board newBoard= Board.builder().updateDTO(updateDTO).build();
+            Board newBoard = Board.builder().updateDTO(newDTO).build();
             boardRepository.save(newBoard);
 
         } catch (IllegalArgumentException e) {
@@ -42,7 +47,21 @@ public class BoardService {
         }
         return 1L;
     }
+    @Transactional
+    public Long validatePassword(Long id, Map map) throws Exception {
+        try {
+            Optional<Board> optional = boardRepository.findById(id);
+            Board oldBoard;
+            if (optional.isPresent()) {
+                oldBoard = optional.get();
+            } else throw new Exception();
+            String oldPassword=oldBoard.getPassword();
 
+            updateValidation.validatePassword(id,map,oldPassword);
 
-
+        } catch (IllegalArgumentException e) {
+            throw new IllegalAccessException("에러메세지");
+        }
+        return 1L;
+    }
 }
