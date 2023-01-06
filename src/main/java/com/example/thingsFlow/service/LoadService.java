6e2dfc0e19470fa802053cebc2412fea5cf778f0
dong yeol <ehfql6363@naver.com>
2023-01-07 00:16:12 +0900

    FEAT(LoadService.java) : 반환 객체 변경 Page -> Slice
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/service/LoadService.java
+++ src/main/java/com/example/thingsFlow/service/LoadService.java
@@ -3,8 +3,8 @@ package com.example.thingsFlow.service;
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,7 +16,7 @@ public class LoadService {
     private BoardRepository boardRepository;
 
     @Transactional
-    public Page<Board> load(Pageable pageable) {
+    public Slice<Board> load(Pageable pageable) {
         return boardRepository.findAll(pageable);
     }
 }
