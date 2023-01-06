cb9cdd23fd734c083c3890b92cffea2b9454beef
jojeongbin <cjb1009@naver.com>
2023-01-06 16:55:49 +0900

    FEAT(Board.java) : 컬럼명, 데이터타입 설정

--- src/main/java/com/example/thingsFlow/entity/Board.java
+++ src/main/java/com/example/thingsFlow/entity/Board.java
@@ -1,4 +1,26 @@
 package com.example.thingsFlow.entity;
+import com.example.thingsFlow.dto.BoardDTO;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Board {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime time;
+
+
 }
