0ce4f51119f235805f10045d7beb4d5579de4340
jojeongbin <cjb1009@naver.com>
2023-01-07 03:15:07 +0900

    FEAT(UpdateValidation.java) : password 검증 클래스 생성

--- src/main/java/com/example/thingsFlow/validation/UpdateValidation.java
+++ src/main/java/com/example/thingsFlow/validation/UpdateValidation.java
@@ -1,2 +1,33 @@
-package com.example.thingsFlow.validation;public class UpdateValidation {
+package com.example.thingsFlow.validation;
+
+import com.example.thingsFlow.dto.UpdateDTO;
+import com.example.thingsFlow.entity.Board;
+import com.example.thingsFlow.service.UpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class UpdateValidation {
+
+    public void validatePassword(Board oldBoard, UpdateDTO updateDTO){
+      if(updateDTO.getPassword().equals(oldBoard.getPassword())){
+          return;
+      }
+      else {
+          throw new IllegalArgumentException("비밀번호가 틀렸습니다");
+      }
+
+    }
+
+    public void validatePassword(Long id, Map map, String oldPassword){
+        if(oldPassword.equals(map.get("password"))){
+            return;
+        }
+        else {
+            throw new IllegalArgumentException("비밀번호가 틀렸습니다");
+        }
+
+    }
 }
