08d68f14b770f1caaab0d5c801bff6350a7eae4c
jojeongbin <cjb1009@naver.com>
2023-01-06 16:58:30 +0900

    BoardDelete 추가

--- src/main/java/com/example/thingsFlow/validation/service/BoardService.java
+++ src/main/java/com/example/thingsFlow/validation/service/BoardService.java
@@ -1,4 +1,36 @@
-package com.example.thingsFlow.service;
+package com.example.thingsFlow.validation.service;
 
+import com.example.thingsFlow.entity.Board;
+import com.example.thingsFlow.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public boolean boardDelete(long id, String password) {
+        Optional<Board> boardEntityWrapper = boardRepository.findById(id);
+        if (boardEntityWrapper.isPresent()) {
+            Board board = boardEntityWrapper.get();
+            if (password.equalsIgnoreCase(board.getPassword())) {
+                boardRepository.deleteById(id);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            System.out.println("존재하지 않는 게시글 삭제 시도");
+            return false;
+        }
+
+
+    }
 }
