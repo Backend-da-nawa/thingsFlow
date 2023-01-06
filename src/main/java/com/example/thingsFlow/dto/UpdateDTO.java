866f0bd2be68b67f96f97ed27b47bb4898d283e4
jojeongbin <cjb1009@naver.com>
2023-01-07 03:07:06 +0900

    REFACTOR(UpdateDTO) : 이름 변경 (BoardDTO ->UpdateDTO)

--- src/main/java/com/example/thingsFlow/dto/UpdateDTO.java
+++ src/main/java/com/example/thingsFlow/dto/UpdateDTO.java
@@ -2,13 +2,17 @@ package com.example.thingsFlow.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
+@Getter
 public class UpdateDTO {
     private Long id;
     private String title;
     private String content;
     private String password;
+
 
     @Builder
     public UpdateDTO(Long id,String title, String content, String password) {
@@ -16,5 +20,6 @@ public class UpdateDTO {
         this.title = title;
         this.content = content;
         this.password = password;
+
     }
 }
