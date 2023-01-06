79d5878478aebb54888bfeeaef22fe6c03044c30
dong yeol <ehfql6363@naver.com>
2023-01-07 00:17:01 +0900

    FEAT(SwaggerConfig.java) : commonInfo 메서드 변경

--- src/main/java/com/example/thingsFlow/swagger/SwaggerConfig.java
+++ src/main/java/com/example/thingsFlow/swagger/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.example.thingsFlow.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,29 +10,38 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
+
 @Configuration
 @EnableSwagger2
-public class Config {
+public class SwaggerConfig {
 
     private static final String API_NAME = "Board API";
     private static final String NAME = "Dong-Yeol";
     private static final String URL = "https://www.notion.so/d3996619afd147a8a1afa0f1d1b1c283?v=db83a96204eb415c9b5095d5507cfd59";
     private static final String E_MAIL = "ehfql6363@naver.com";
     private static final String API_DESCRIPTION = "띵스플로우 API 명세서";
+    private String version;
 
     private ApiInfo commonInfo() {
-        return new ApiInfoBuilder()
-                .title(API_NAME)
-                .description(API_DESCRIPTION)
-                .contact(new Contact(NAME, URL, E_MAIL))
-                .version("1.0")
-                .build();
+        return new ApiInfo(
+                API_NAME,
+                API_DESCRIPTION,
+                version,
+                "https://naver.com",
+                new Contact(NAME, URL, E_MAIL),
+                "License",
+                "https://naver.com",
+                new ArrayList<>());
     }
 
     @Bean
-    public Docket allApi() {
+    public Docket apiV1() {
+        version = "V1";
+
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("USER")
+                .groupName(version)
                 .useDefaultResponseMessages(false)
                 .select()
                 //.apis(RequestHandlerSelectors.any())
