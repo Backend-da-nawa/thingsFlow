d335466d7184f93b13ff6215e7d368c5a2683154
jojeongbin <cjb1009@naver.com>
2023-01-06 22:16:24 +0900

    FEAT(UpdaeDTO.java) : builder 추가

--- src/main/java/com/example/thingsFlow/dto/UpdateDTO.java
+++ src/main/java/com/example/thingsFlow/dto/UpdateDTO.java
@@ -1,4 +1,20 @@
 package com.example.thingsFlow.dto;
 
-public class BoardDTO {
+import lombok.*;
+
+@NoArgsConstructor
+@Data
+public class UpdateDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private String password;
+
+    @Builder
+    public UpdateDTO(Long id,String title, String content, String password) {
+        this.id=id;
+        this.title = title;
+        this.content = content;
+        this.password = password;
+    }
 }
