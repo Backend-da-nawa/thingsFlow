7afe9245fbb0d168f817553384c60df12b50e177
jojeongbin <cjb1009@naver.com>
2023-01-06 22:11:53 +0900

    FEAT(Board.java) : 테이블 내용 추가

--- src/main/java/com/example/thingsFlow/entity/Board.java
+++ src/main/java/com/example/thingsFlow/entity/Board.java
@@ -1,4 +1,34 @@
 package com.example.thingsFlow.entity;
+import com.example.thingsFlow.dto.UpdateDTO;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Board {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime time;
+
+    @Builder
+    public Board(UpdateDTO updateDTO) {
+        this.id = getId();
+        this.title=getTitle();
+        this.content=getContent();
+        this.password=getPassword();
+    }
+
+
 }
