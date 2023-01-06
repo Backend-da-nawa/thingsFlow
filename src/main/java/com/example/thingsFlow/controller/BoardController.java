3866a19b31998961e6ac8aaf1b9fb9814bb3d03a
jojeongbin <cjb1009@naver.com>
2023-01-06 16:53:38 +0900

    REFATOR(BoardDTO)

--- src/main/java/com/example/thingsFlow/controller/BoardController.java
+++ src/main/java/com/example/thingsFlow/controller/BoardController.java
@@ -1,4 +1,20 @@
 package com.example.thingsFlow.controller;
 
+import com.example.thingsFlow.validation.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class BoardController {
+    private BoardService boardService;
+
+    @DeleteMapping("/post/delete/{id}")
+    public Long delete(@PathVariable("id") long id, @RequestParam("password") String password){
+        boolean deleteCheckFlag = boardService.boardDelete(id, password);
+        if(deleteCheckFlag == false){
+            System.out.println("비밀번호가 일치하지 않습니다");
+            return 1L;
+        }
+        return 2L;
+    }
 }
--- src/main/java/com/example/thingsFlow/dto/BoardDTO.java
+++ src/main/java/com/example/thingsFlow/dto/BoardDTO.java
@@ -1,4 +1,5 @@
 package com.example.thingsFlow.dto;
 
 public class BoardDTO {
+
 }
