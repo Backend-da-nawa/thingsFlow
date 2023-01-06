4572dd3bab28be14d3cae032ebd56be194e2f18b
dong yeol <ehfql6363@naver.com>
2023-01-06 09:50:35 +0900

    FEAT(LoadService.java) : load 메서드 생성

--- src/main/java/com/example/thingsFlow/service/LoadService.java
+++ src/main/java/com/example/thingsFlow/service/LoadService.java
@@ -1,7 +1,22 @@
 package com.example.thingsFlow.service;
 
+import com.example.thingsFlow.entity.Board;
+import com.example.thingsFlow.repository.BoardRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@AllArgsConstructor
 public class LoadService {
+
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public Page<Board> load(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
 }
