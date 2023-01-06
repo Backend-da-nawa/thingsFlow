71cd1427367271dcf56e531534849911dd39deab
dong yeol <ehfql6363@naver.com>
2023-01-07 00:12:56 +0900

    FEAT(LoadController.java) : Swagger 관련 어노테이션 추가

--- src/main/java/com/example/thingsFlow/controller/LoadController.java
+++ src/main/java/com/example/thingsFlow/controller/LoadController.java
@@ -2,20 +2,27 @@ package com.example.thingsFlow.controller;
 
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.service.LoadService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(("/api"))
 @AllArgsConstructor
+@Api(tags = {"api"}, value = "게시글 조회 API 정보를 제공하는 Controller")
 public class LoadController {
 
     private LoadService loadService;
 
     @GetMapping("/load")
-    public Page<Board> load(final Pageable pageable) {
+    @ApiOperation(value = "조회할 게시글을 페이지로 반환하는 메서드")
+    public Slice<Board> load(final Pageable pageable) {
         return loadService.load(pageable);
     }
 }
