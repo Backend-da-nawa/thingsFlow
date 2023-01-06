657dccd63ea147979c9e8fedb392ebf298eeb3bb
jojeongbin <cjb1009@naver.com>
2023-01-06 22:09:52 +0900

    FEAT(BoardController.java) : edit 메소드 추가

--- src/main/java/com/example/thingsFlow/controller/BoardController.java
+++ src/main/java/com/example/thingsFlow/controller/BoardController.java
@@ -1,4 +1,21 @@
 package com.example.thingsFlow.controller;
 
+import com.example.thingsFlow.dto.UpdateDTO;
+import com.example.thingsFlow.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class BoardController {
+    private BoardService boardService;
+    @GetMapping(value = "/post/edit/{id}")
+    public Long edit(@PathVariable("id") long id, @RequestParam("password") String password) throws Exception{
+        return boardService.updateBoard(id,password);
+    }
+
+
+
 }
