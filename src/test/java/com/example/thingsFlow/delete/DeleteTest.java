1204902e690d56996fd4f753a521a6708d2a6305
dong yeol <ehfql6363@naver.com>
2023-01-10 01:04:33 +0900

    FEAT(DeleteTest.java) : 게시판 글 삭제 테스트 코드 변경, 초기 설정 메서드 추가
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/test/java/com/example/thingsFlow/delete/DeleteTest.java
+++ src/test/java/com/example/thingsFlow/delete/DeleteTest.java
@@ -1,29 +1,31 @@
 package com.example.thingsFlow.delete;
 
 
-import com.example.thingsFlow.dto.BoardDTO;
+import com.example.thingsFlow.dto.DeleteBoardDTO;
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
 import org.junit.After;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("게시판 테스트")
+@ExtendWith(SpringExtension.class)
 public class DeleteTest {
     @LocalServerPort
     private int port;
@@ -41,21 +43,39 @@ public class DeleteTest {
 
     @Test
     public void 게시판_글_삭제(){
+        // init
+        long id = setUpOneBoard().getId();
 
-        Board savedBoard = boardRepository.save(Board.builder()
-                .id(1L)
-                .title("인사드립니다")
-                .content("안녕하세요 만나서 반갑습니다.")
-                .password("1234")
+        // given
+        Map<String, Object> map = new HashMap();
+        map.put("password", "1111");
+
+        String url = "http://localhost:" + port + "/api/board/" + id;
+
+        // when
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(map);
+
+        ResponseEntity<Board> responseEntity = restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, Board.class);
+
+        // then
+        Assertions.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+
+        List<Board> list = boardRepository.findAll();
+        Assertions.assertEquals(list.size(), 0);
+    }
+
+    private Board setUpOneBoard() {
+        long id = 1;
+        String title = "Title_";
+        String content = "Content_";
+        String password = "1111";
+
+        return boardRepository.save(Board.builder()
+                .deleteBoardDTO(DeleteBoardDTO.builder()
+                        .id(id)
+                        .title(title + id)
+                        .content(content + id)
+                        .password(password).build())
                 .build());
-
-
-        String url = "http://localhost:" + port + "/board/{id}";
-
-        UriComponents uri = UriComponentsBuilder.fromUriString(url).encode().build();
-        restTemplate.delete(uri.toUri());
-
-        List<Board> deleted =boardRepository.findAll();
-        assertThat(deleted.size()).isEqualTo(0);
     }
 }
