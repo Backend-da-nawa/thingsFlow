3b8072cfac87296661b9d88df9d83882d17558f2
dong yeol <ehfql6363@naver.com>
2023-01-09 14:18:36 +0900

    FEAT(DeleteService.java) : getBoard 메서드 로직 최적화
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/service/DeleteService.java
+++ src/main/java/com/example/thingsFlow/service/DeleteService.java
@@ -1,4 +1,4 @@
-package com.example.thingsFlow.validation.service;
+package com.example.thingsFlow.service;
 
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
@@ -19,19 +19,15 @@ public class DeleteService {
 
     @Transactional
     public Long validatePassword(Long id, Map map){
-        String oldPassword=getBoard(id).getPassword();
-        deleteValidation.validatePassword(map,oldPassword);
+        String oldPassword = getBoard(id).getPassword();
+        deleteValidation.validatePassword(map, oldPassword);
         boardRepository.deleteById(id);
         return 1L;
     }
 
     private Board getBoard(Long id){
-        Optional<Board> optional=boardRepository.findById(id);
-        Board oldBoard;
-        if(optional.isPresent()){
-            oldBoard=optional.get();
-        }else throw new IllegalArgumentException("[ERROR] 존재하지 않는 게시물입니다");
-        return oldBoard;
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + id));
     }
     
 }
