2edfb99093ead155d32d1c238c4c0aaf8ac71f27
jojeongbin <cjb1009@naver.com>
2023-01-09 15:13:35 +0900

    테스트 위한 의존성 추가

--- src/main/java/com/example/thingsFlow/dto/BoardDTO.java
+++ src/main/java/com/example/thingsFlow/dto/BoardDTO.java
@@ -1,5 +1,29 @@
 package com.example.thingsFlow.dto;
 
-public class BoardDTO {
+import com.example.thingsFlow.entity.Board;
+import lombok.*;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+@Data
+public class InsertDTO {
+    private String title;
+    private String content;
+    private String password;
+    private String weather;
+    private String uploaded;
+
+    public InsertDTO(Board board) {
+        this(board.getTitle(), board.getContent(), board.getPassword(), board.getWeather(), board.getUploaded());
+    }
+
+    @Builder
+    public InsertDTO(String title, String content, String password, String weather, String uploaded) {
+        this.title = title;
+        this.content = content;
+        this.password = password;
+        this.weather = weather;
+        this.uploaded = uploaded;
+    }
 }
