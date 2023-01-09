087dffa8963b1b0bfb79ed3271793236e077458b
dong yeol <ehfql6363@naver.com>
2023-01-09 14:16:40 +0900

    FEAT(DeleteController.java) : Autowired 어노테이션 삭제, RequiredArgsConstructor 어노테이션 추가
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/controller/DeleteController.java
+++ src/main/java/com/example/thingsFlow/controller/DeleteController.java
@@ -1,14 +1,16 @@
 package com.example.thingsFlow.controller;
 
-import com.example.thingsFlow.validation.service.DeleteService;
+import com.example.thingsFlow.service.DeleteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 public class DeleteController {
-    private DeleteService deleteService;
+    private final DeleteService deleteService;
 
     @DeleteMapping("/board/{id}")
     public Long validatePassword(@PathVariable("id") long id, @RequestBody Map<String, Object> map) {
