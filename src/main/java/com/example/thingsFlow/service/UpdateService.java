52812b4c14cd7e372168c616fb5cddd134b30e59
dong yeol <ehfql6363@naver.com>
2023-01-10 20:56:39 +0900

    FEAT(UpdateService.java) : 매게변수 변경 및 코드 최적화
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/service/UpdateService.java
+++ src/main/java/com/example/thingsFlow/service/UpdateService.java
@@ -1,46 +1,36 @@
 package com.example.thingsFlow.service;
 
+import com.example.thingsFlow.password.Encryption;
 import com.example.thingsFlow.dto.UpdateDTO;
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
-import com.example.thingsFlow.validation.UpdateValidation;
+import com.example.thingsFlow.password.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Map;
-import java.util.Optional;
-
 
 @Service
 @RequiredArgsConstructor
 public class UpdateService {
     private final BoardRepository boardRepository;
-    private final UpdateValidation updateValidation;
+    private final Validation validation;
     @Transactional
     public Board updateBoard(UpdateDTO updateDTO) {
         Board board = boardRepository.findById(updateDTO.getId())
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + updateDTO.getId()));
-
-        updateValidation.validatePassword(board, updateDTO);
-
-        board.update(updateDTO);
-        return board;
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id = " + updateDTO.getId()));
+        if (validation.checkHashedPassword(updateDTO.getPassword(), board.getPassword())) {
+            board.update(updateDTO);
+            return board;
+        } else throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
     }
     @Transactional
     public Long validatePassword(Long id, Map map) {
-        String oldPassword = getBoard(id).getPassword();
-
-        updateValidation.validatePassword(map, oldPassword);
-
-        return 1L;
-    }
-
-    private Board getBoard(Long id) {
-        Optional<Board> optional = boardRepository.findById(id);
-        Board oldBoard;
-        if (optional.isPresent()) {
-            oldBoard = optional.get();
-        } else throw new IllegalArgumentException("[ERROR] 해당하는 게시글이 없습니다.");
-        return oldBoard;
+        if (validation.checkHashedPassword(
+                (String) map.get("password"),
+                boardRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당하는 게시글이 존재하지 않습니다. id = " + id)).getPassword())) {
+            return 1L;
+        } else throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
     }
 }
