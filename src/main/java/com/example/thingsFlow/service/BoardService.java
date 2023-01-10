3453656ec26a0934603a9c31396900aa5febbfd6
dong yeol <ehfql6363@naver.com>
2023-01-10 14:34:22 +0900

    FEAT(BoardService.java) : insert 메서드 분리

--- src/main/java/com/example/thingsFlow/service/BoardService.java
+++ src/main/java/com/example/thingsFlow/service/BoardService.java
@@ -4,6 +4,7 @@ import com.example.thingsFlow.dto.InsertDTO;
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
 import com.example.thingsFlow.parser.WeatherAPIParser;
+import io.swagger.models.auth.In;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
@@ -23,18 +24,20 @@ public class BoardService {
     }
 
     @Transactional
-    public InsertDTO insert(InsertDTO insertDTO) {
+    public Board insert(InsertDTO insertDTO) {
+        InsertDTO dto = getDTO(insertDTO);
+        Board board = new Board(dto);
+        boardRepository.save(board);
+        return board;
+    }
+
+    private InsertDTO getDTO(InsertDTO insertDTO) {
         WeatherAPIParser weatherAPIParser = WeatherAPIParser.getInstance();
         String weather = weatherAPIParser.getCurrentWeather();
-
-        Board board = new Board(
-                insertDTO.getTitle(),
-                insertDTO.getContent(),
-                hashingPassword(insertDTO.getPassword()),
-                weather
-        );
-
-        boardRepository.save(board);
-        return new InsertDTO(board);
+        return InsertDTO.builder()
+                .title(insertDTO.getTitle())
+                .content(insertDTO.getContent())
+                .password(insertDTO.getPassword())
+                .weather(weather).build();
     }
 }
