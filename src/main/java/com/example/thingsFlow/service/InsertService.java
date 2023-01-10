a065293ef9023f5299a0d4d9b5d5394974407b8a
dong yeol <ehfql6363@naver.com>
2023-01-10 20:54:39 +0900

    REFACTOR(InsertService.java) : 코드 최적화
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/thingsFlow/service/InsertService.java
+++ src/main/java/com/example/thingsFlow/service/InsertService.java
@@ -1,11 +1,11 @@
 package com.example.thingsFlow.service;
 
+import com.example.thingsFlow.password.Encryption;
 import com.example.thingsFlow.dto.InsertDTO;
 import com.example.thingsFlow.entity.Board;
 import com.example.thingsFlow.repository.BoardRepository;
 import com.example.thingsFlow.parser.WeatherAPIParser;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,21 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class InsertService {
     private final BoardRepository boardRepository;
-
-    private String hashingPassword(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt());
-    }
-
-    private boolean checkHashedPassword(String hashedPassword, String storedPassword) {
-        return BCrypt.checkpw(hashedPassword, storedPassword);
-    }
+    private final Encryption encryption;
 
     @Transactional
     public Board insert(InsertDTO insertDTO) {
         InsertDTO dto = getDTO(insertDTO);
         Board board = new Board(dto);
-        boardRepository.save(board);
-        return board;
+        return boardRepository.save(board);
     }
 
     private InsertDTO getDTO(InsertDTO insertDTO) {
@@ -36,7 +28,7 @@ public class InsertService {
         return InsertDTO.builder()
                 .title(insertDTO.getTitle())
                 .content(insertDTO.getContent())
-                .password(hashingPassword(insertDTO.getPassword()))
+                .password(encryption.hashingPassword(insertDTO.getPassword()))
                 .weather(weather).build();
     }
 }
