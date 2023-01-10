9020f92dd9882ac374723bc022f53d148f46bc3a
dong yeol <ehfql6363@naver.com>
2023-01-10 14:06:57 +0900

    FEAT(InsertDTO.java) : uploaded 속성 삭제

--- src/main/java/com/example/thingsFlow/dto/InsertDTO.java
+++ src/main/java/com/example/thingsFlow/dto/InsertDTO.java
@@ -7,23 +7,18 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 public class InsertDTO {
     private String title;
     private String content;
     private String password;
     private String weather;
-    private String uploaded;
-
-    public InsertDTO(Board board) {
-        this(board.getTitle(), board.getContent(), board.getPassword(), board.getWeather(), board.getUpdatedTime().toString());
-    }
 
     @Builder
-    public InsertDTO(String title, String content, String password, String weather, String uploaded) {
+    public InsertDTO(String title, String content, String password, String weather) {
         this.title = title;
         this.content = content;
         this.password = password;
         this.weather = weather;
-        this.uploaded = uploaded;
     }
 }
